6327efa22ab0a8a6c3a96539f9fa0afd93a87220
priyansh <priyanshmathur16@gmail.com>
2022-07-18 09:53:29 +0530

    Initial commit

---
+++ gfg/gfg/info.py
@@ -0,0 +1,5 @@
+EMAIL_USE_TLS=True
+EMAIL_HOST ='sntp.gmail.com'
+EMAIL_HOST_USER='priyanshmathur16@gmail.com'
+EMAIL_HOST_PASSWORD='#dino2002'
+EMAIL_PORT=587
---
+++ gfg/poll/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render
+
+# Create your views here.
+from tkinter.messagebox import QUESTION
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+from .forms import CreatePollForm
+from .models import Poll
+
+def cast(request):
+    polls = Poll.objects.all()
+    context = {
+        'polls' : polls
+    }
+    return render(request, 'poll/cast.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        form = CreatePollForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('cast')
+    else:
+        form = CreatePollForm()
+    context = {
+        'form' : form
+    }
+    return render(request, 'poll/create.html', context)
+
+def vote(request, poll_id):
+    poll = Poll.objects.get(pk=poll_id)
+
+    if request.method == 'POST':
+
+        selected_option = request.POST['poll']
+        if selected_option == 'option1':
+            poll.option_one_count += 1
+        elif selected_option == 'option2':
+            poll.option_two_count += 1
+        elif selected_option == 'option3':
+            poll.option_three_count += 1
+        elif selected_option == 'option4':
+            poll.option_four_count += 1
+        else:
+            return HttpResponse(400, 'Invalid form')
+
+        poll.save()
+
+        return redirect('results', poll.id)
+
+    context = {
+        'poll' : poll
+    }
+    return render(request, 'poll/vote.html', context)
+
+def results(request, poll_id):
+    poll = Poll.objects.get(pk=poll_id)
+    context = {
+        'poll' : poll
+    }
+    return render(request, 'poll/results.html', context)
