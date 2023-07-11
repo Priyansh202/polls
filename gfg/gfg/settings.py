6a0bd20d5af398b02ab90f93153fc493f809fa3c
Priyansh202 <96569257+Priyansh202@users.noreply.github.com>
2023-07-11 10:48:59 +0530

    x

--- gfg/gfg/settings.py
+++ gfg/gfg/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-2607qp+mr^2%5$cl^&z^ynt71pauknz-3eszkawfu^_i0kmd-3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','now.sh']
 
 
 # Application definition
